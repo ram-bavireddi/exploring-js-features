5a81ccfaa0fac8e6090f723dc624b46ce7c7d4da
Ram Bavireddi <ramu.bavireddi@gmail.com>
2019-07-10 06:41:00 +0530

    yield and generator funcations

--- yield-based.js
+++ yield-based.js
@@ -1,13 +1,18 @@
 import { api } from './api.js';
 export function yieldFun(data1El, data2El, data3El) {
-    const itr = _yieldFun();
-    data1El.innerHTML = itr.next();
-    data2El.innerHTML = itr.next();
-    data3El.innerHTML = itr.next();
+    co(_yieldFun.call(null, data1El, data2El, data3El)).then(_ => {
+        console.log('logged');
+    })
 }
 
-function* _yieldFun() {
-    yield api.invokeApi(2000, 'Data 1');
-    yield api.invokeApi(4000, 'Data 2');
-    yield api.invokeApi(2000, 'Data 3');
+function* _yieldFun(data1El, data2El, data3El) {
+    yield api.invokeApi(2000, 'Data 1').then(data => {
+        data1El.innerHTML = data;
+    });
+    yield api.invokeApi(4000, 'Data 2').then(data => {
+        data2El.innerHTML = data;
+    });
+    yield api.invokeApi(2000, 'Data 3').then(data => {
+        data3El.innerHTML = data;
+    });
 }
